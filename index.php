c070d5d78e2f2691c6a9133069452675c5c53920
EfeKrK <efekurkcu00@mail.com>
2024-09-26 19:14:35 +0300

    footer eklendi - Navbar tasarımı geliştirildi

--- index.php
+++ index.php
@@ -5,31 +5,41 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/footer.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 <!-- Navbar -->
 
-<nav class="navbar navbar-expand-lg navbar-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">
-                <img src="images/logo.png" alt="" width="100" height="75">
-            </a>
-            <a class="navbar-brand header-text" href="#">Altınay Savunma Teknolojileri A.Ş.</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav ms-auto me-5">
-                    <a class="nav-link active" aria-current="page" href="#">Ana Sayfa</a>
-                    <a class="nav-link" href="projeler.php">Projeler</a>
-                    <a class="nav-link" href="Medya.php">Medya</a>
-                    <a class="nav-link" href="hakkimizda.php">Hakkımızda</a>
-                    <a class="nav-link" href="iletisim.php">İletişim</a>
-                </div>
+<nav class="navbar navbar-expand-lg navbar-light d-flex justify-content-between">
+    <div class="container-fluid">
+        <a class="navbar-brand header-text" href="#">HAS GENESIS</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav mx-auto justify-content-center" id="nav-link-main">
+                <a class="nav-link active" aria-current="page" href="#">Ana Sayfa</a>
+                <a class="nav-link" href="projeler.php">HAS CREW</a>
+                <a class="nav-link" href="Medya.php">PROJELER</a>
+                <a class="nav-link" href="hakkimizda.php">HABER BÜLTENİ</a>
+                <a class="nav-link" href="iletisim.php">ORGANİZASYONLAR</a>
+                <a class="nav-link" href="iletisim.php">BİZ KİMİZ ?</a>
+            </div>
+            <div class="navbar-nav">
+                <a href="login.php" class="btn-login nav-link">Giriş Yap</a>
+                <a href="register.php" class="btn-register nav-link">Kayıt Ol</a>
             </div>
         </div>
-    </nav>
+    </div>
+</nav>
 
+<footer class="footer mt-auto py-2">
+    <div class="footer-container text-center">
+        <span class="text-muted">HAS GENESIS &copy; 2024. Tüm hakları saklıdır.</span>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
