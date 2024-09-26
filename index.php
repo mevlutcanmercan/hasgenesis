3312e975e521b908f99fc2b45f6e47b6cfd300af
EfeKrK <efekurkcu00@mail.com>
2024-09-26 17:23:37 +0300

    navbar taslak eklendi

--- index.php
+++ index.php
@@ -4,8 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/navbar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    
+<!-- Navbar -->
+
+<nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/logo.png" alt="" width="100" height="75">
+            </a>
+            <a class="navbar-brand header-text" href="#">Altınay Savunma Teknolojileri A.Ş.</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ms-auto me-5">
+                    <a class="nav-link active" aria-current="page" href="#">Ana Sayfa</a>
+                    <a class="nav-link" href="projeler.php">Projeler</a>
+                    <a class="nav-link" href="Medya.php">Medya</a>
+                    <a class="nav-link" href="hakkimizda.php">Hakkımızda</a>
+                    <a class="nav-link" href="iletisim.php">İletişim</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
 </body>
 </html>
