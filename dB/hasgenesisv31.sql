ff35a2ccc5f7442eebe250a6ae1a87b45a0a2270
Can1903a <atlasis_3131@hotmail.com>
2024-09-27 14:42:58 +0300

    asd

--- dB/hasgenesisv31.sql
+++ dB/hasgenesisv31.sql
@@ -16,6 +16,33 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `main_page_sliders`
+--
+
+DROP TABLE IF EXISTS `main_page_sliders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `main_page_sliders` (
+  `id` int NOT NULL,
+  `title` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `summary` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `image_path` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `link` text COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `main_page_sliders`
+--
+
+LOCK TABLES `main_page_sliders` WRITE;
+/*!40000 ALTER TABLE `main_page_sliders` DISABLE KEYS */;
+INSERT INTO `main_page_sliders` VALUES (4,'deneme','denemedeneme','images/denemee.jpeg','instagram.com','2024-09-27 07:46:31'),(5,'deneme','denemedeneme','images/deneme.jpg','hasgenesıs.com','2024-09-27 07:49:25');
+/*!40000 ALTER TABLE `main_page_sliders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -24,13 +51,15 @@ DROP TABLE IF EXISTS `users`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
   `id_users` int NOT NULL AUTO_INCREMENT,
-  `password_users` varchar(45) NOT NULL,
+  `password_users` varchar(255) NOT NULL,
   `mail_users` varchar(45) NOT NULL,
   `isAdmin` tinyint(1) DEFAULT '0',
   `name_users` varchar(45) NOT NULL,
   `surname_users` varchar(45) NOT NULL,
+  `telefon` varchar(45) DEFAULT NULL,
+  `birthday_users` date DEFAULT NULL,
   PRIMARY KEY (`id_users`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +68,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (2,'12345','deneme2@mail.com',0,'user','surname');
+INSERT INTO `users` VALUES (3,'$2y$10$JKe5zIOCWpPahiACb8el6ugrtpPp2WPdny3O0iLqcUWJHuMQ/eMQO','deneme5@mail.com',0,'Mevlutcan','Mercan','55555','2019-05-27'),(13,'$2y$10$EiUbBaw1oqK.pZHTvRm7C.DjR.oPTlEQCC/5wBQTZXtfZJkQ3sCTW','deneme31@mail.com',0,'Mevlutcan','Mercan','1436436','2024-08-31'),(14,'$2y$10$fy5yweDra8ZggKE78zqCmuhk6oG0voOmNhz4eskM9JiAXPrWLVmoq','deneme32@mail.com',0,'asdasd','asdasd','123123','2024-09-12');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-26 20:20:33
+-- Dump completed on 2024-09-27 14:42:20
