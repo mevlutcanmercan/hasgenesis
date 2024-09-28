64e855c836cb0798df10dbb3da33dc42fa55fa01
Can1903a <atlasis_3131@hotmail.com>
2024-09-28 13:16:54 +0300

    x

--- navbar.php
+++ navbar.php
@@ -49,7 +49,6 @@ if (isset($_SESSION['id_users'])) {
     <link rel="stylesheet" href="css/navbar.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <title>Document</title>
 </head>
 <body>
     
