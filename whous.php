027822ea0e9958ef44b00675630755ace86ae3c5
EfeKrK <efekurkcu070@mail.com>
2024-10-02 15:48:48 +0300

    hata giderme

--- whous.php
+++ whous.php
@@ -3,11 +3,17 @@ include 'dB/database.php'; // Veritabanı bağlantısını buradan alıyoruz
 include 'navbar.php';
 include 'bootstrap.php';
 
-
 // Veritabanından bilgileri çek
 $query = "SELECT * FROM who_us LIMIT 1";
 $result = $conn->query($query); // mysqli bağlantısını kullanıyoruz
-$data = $result->fetch_all(MYSQLI_ASSOC); // Sonuçları al
+
+$data = []; // Sonuçları tutmak için boş bir dizi oluştur
+
+if ($result) {
+    while ($row = $result->fetch_assoc()) { // Her satırı al ve ilişkilendirilmiş diziye ekle
+        $data[] = $row;
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,25 +29,27 @@ $data = $result->fetch_all(MYSQLI_ASSOC); // Sonuçları al
 
 <div class="who-us-container">
     <!-- Veritabanındaki her satırı çek -->
-    <?php foreach ($data as $row): ?>
-        <!-- Soldaki resim -->
-        <img src="<?php echo $row['logo_path']; ?>" alt="Biz Kimiz Logo">
-        
-        <!-- Sağdaki içerik -->
-        <div class="who-us-content">
-            <h1><?php echo $row['header']; ?></h1>
-            <p><?php echo $row['text']; ?></p>
+    <?php if (!empty($data)): ?>
+        <?php foreach ($data as $row): ?>
+            <!-- Soldaki resim -->
+            <img src="<?php echo htmlspecialchars($row['logo_path']); ?>" alt="Biz Kimiz Logo">
             
-            <!-- Sosyal medya ikonları -->
-            <div class="social-icons">
-                <a href="https://www.facebook.com/hasdownhill" target="_blank"><i class='bx bxl-facebook'></i></a>
-                <a href="https://www.instagram.com/hasdownhill/" target="_blank"><i class='bx bxl-instagram'></i></a>
+            <!-- Sağdaki içerik -->
+            <div class="who-us-content">
+                <h1><?php echo htmlspecialchars($row['header']); ?></h1>
+                <p><?php echo htmlspecialchars($row['text']); ?></p>
+                
+                <!-- Sosyal medya ikonları -->
+                <div class="social-icons">
+                    <a href="https://www.facebook.com/hasdownhill" target="_blank"><i class='bx bxl-facebook'></i></a>
+                    <a href="https://www.instagram.com/hasdownhill/" target="_blank"><i class='bx bxl-instagram'></i></a>
+                </div>
             </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>İçerik bulunamadı.</p> <!-- Varsayılan içerik -->
+    <?php endif; ?>
 </div>
-
-
 
 <script>
 // Sayfa yüklendiğinde çalışacak fonksiyon
@@ -67,16 +75,11 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 </script>
 
-
-
-
-
 <footer class="footer mt-auto py-2">
     <div class="footer-container text-center">
         <span class="text-muted">HAS GENESIS &copy; 2024. Tüm hakları saklıdır.</span>
     </div>
 </footer>
-
 
 </body>
 </html>
