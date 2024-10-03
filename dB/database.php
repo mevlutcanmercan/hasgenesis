f90a32cfffe6f5af6d3db8b37e795820fd191b6a
EfeKrK <efekurkcu070@mail.com>
2024-10-03 17:22:08 +0300

    database

--- dB/database.php
+++ dB/database.php
@@ -1,12 +1,16 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "1234";
+$password = "";
 $dbname = "hasgenesis";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Bağlantı kontrolü
 if ($conn->connect_error) {
     die("Bağlantı başarısız: " . $conn->connect_error);
-}   
+}
+
+// Karakter setini UTF-8 olarak ayarlama
+$conn->set_charset("utf8");
 ?>
