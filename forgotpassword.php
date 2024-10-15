092a62aa2d8184adf337e2c28eda642dea9da70f
EfeKrK <efekurkcu070@gmail.com>
2024-10-15 15:49:15 +0300

    şifremi unuttum ve ayarlar

--- forgotpassword.php
+++ forgotpassword.php
@@ -1,5 +1,3 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Şifremi Unuttum</title>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
     <link rel="stylesheet" href="css/forgotpassword.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <link rel="shortcut icon" href="../images/admin-panel.png" type="image/x-icon">
 </head>
 <body>
@@ -27,11 +26,26 @@
                 <input type="email" name="eposta" placeholder="E-posta Giriniz" required>
                 <i class='bx bx-envelope' style="color:black"></i>
             </div>
-
-            <button type="submit" class="btn">Şifremi Gönder</button>
+            
+            <button type="submit" class="btn">Yeni Şifremi Gönder</button>
         </form>
     </div>
 
-   
+    <!-- Mesajı gösteren JavaScript -->
+    <script>
+        let message = <?php echo json_encode($_SESSION['message'] ?? null); ?>;
+        if (message) {
+            swal({
+                title: message.type === 'success' ? "Başarılı!" : "Hata!",
+                text: message.text,
+                icon: message.type === 'success' ? "success" : "error",
+                button: "Tamam",
+            }).then(() => {
+                // Tamam butonuna tıklandığında sayfayı yenile veya yönlendirin
+                window.location.href = 'forgotpassword.php'; // Yönlendirme adresini buraya ekleyin
+            });
+        }
+    </script>
+
 </body>
 </html>
