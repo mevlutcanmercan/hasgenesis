f5b9a6faf7a4a1cceb193c1d58353fa1dd8c6160
EfeKrK <efekurkcu070@gmail.com>
2024-10-18 15:57:47 +0300

    fix

--- account.php
+++ account.php
@@ -276,7 +276,7 @@ $user_bikes_result = $user_bikes_stmt->get_result();
 <html lang="tr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=700">
     <link rel="stylesheet" href="css/account.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
